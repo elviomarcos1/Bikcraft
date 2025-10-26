9d2a5105757038db7161179522d1360c3905f95d
Marcos <elviomarcosfj@gmail.com>
2025-10-26 11:39:45 -0300

    Fix contact background-color

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('teste')
+const  links = document.querySelectorAll('.header-menu a')
+
+function linkActive(link) {
+    const url = location.href
+    const href = link.href
+    
+    if (url.includes(href)) {
+        link.classList.add("active")
+    }
+}
+
+links.forEach(linkActive)
